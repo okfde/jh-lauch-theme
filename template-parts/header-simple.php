a7a43de349e4efbfd5393eaf4aa3aaf20d2bf367
lislis <mail@lislis.de>
2019-09-09 14:22:28 +0200

    add illustration for faq

--- template-parts/header-simple.php
+++ template-parts/header-simple.php
@@ -1,5 +1,9 @@
 <div class="c-page-header">
   <?php the_title('<h1 class="c-page-title">', '</h1>')?>
-  <div class="c-page-header-illustration"></div>
-  <div class="c-page-header-illustration"></div>
+  <div class="c-page-header-illustration right-one">
+    <img src="<?php echo get_field('illustration_right'); ?>" alt="" width="120">
+  </div>
+  <div class="c-page-header-illustration left-one">
+    <img src="<?php echo get_field('illustration_left'); ?>" alt="" width="300">
+  </div>
 </div>
