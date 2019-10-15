f7730086d0289d71290e3dfdec2cac4cf31ce3c9
lislis <mail@lislis.de>
2019-10-15 16:06:39 +0200

    standard styles

--- page.php
+++ page.php
@@ -15,26 +15,16 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+<div class="wp-styles c-page-standard">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+  <?php
+  while ( have_posts() ) :
+  the_post();
 
-			get_template_part( 'template-parts/content', 'page' );
+  get_template_part( 'template-parts/content', 'page' );
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
+  endwhile; // End of the loop.
+  ?>
+</div>
 <?php
-get_sidebar();
 get_footer();
