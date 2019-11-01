b1c00a334496e4915404290b45d4099a88c81cd9
lislis <mail@lislis.de>
2019-11-01 22:18:10 +0100

    set if

--- single.php
+++ single.php
@@ -32,7 +32,6 @@ the_post(); ?>
           <?php
           $person = get_field('contact_person');
           $img = get_the_post_thumbnail_url( $person, array(50, 50) );
-          if ($person):
           ?>
           <img src="<?php echo $img ?>" alt="Bild von <?php echo get_the_title($person); ?>" class="c-author-image">
           <div class="c-author-text">
@@ -40,7 +39,7 @@ the_post(); ?>
               <time datetime="<?php echo get_the_date( 'Y-m-j'); ?>">
                 <?php echo get_the_date('j. F Y'); ?></time></p>
           </div>
-          <?php endif; ?>
+
         </div>
       </div>
 
