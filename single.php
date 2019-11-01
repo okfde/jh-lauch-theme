ea77f38eecff74270bc09d6565ec389967a78899
lislis <mail@lislis.de>
2019-11-01 22:23:06 +0100

    set if

--- single.php
+++ single.php
@@ -31,11 +31,11 @@ the_post(); ?>
         <div class="c-author">
           <?php
           $person = get_field('contact_person');
-          $img = get_the_post_thumbnail_url( $person, array(50, 50) );
+          $img = get_the_post_thumbnail_url( $person->ID, array(50, 50) );
           ?>
-          <img src="<?php echo $img ?>" alt="Bild von <?php echo get_the_title($person); ?>" class="c-author-image">
+          <img src="<?php echo $img ?>" alt="Bild von <?php echo get_the_title($person->ID); ?>" class="c-author-image">
           <div class="c-author-text">
-            <p>von <?php echo get_the_title($person); ?><br>
+            <p>von <?php echo get_the_title($person->ID); ?><br>
               <time datetime="<?php echo get_the_date( 'Y-m-j'); ?>">
                 <?php echo get_the_date('j. F Y'); ?></time></p>
           </div>
