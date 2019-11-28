770885a547a13ffbbeb8b1dd1371118684bc9f2a
lislis <mail@lislis.de>
2019-11-28 13:23:53 +0100

    add thank you to lab overview

--- lab-overview.php
+++ lab-overview.php
@@ -9,8 +9,7 @@ get_header();
 while ( have_posts() ) :
 the_post();
 get_template_part( 'template-parts/header-alpaka', get_post_type() );
-
-endwhile; ?>
+?>
 
 <section class="c-blog-list is-grid pt-10">
   <?php
@@ -31,6 +30,35 @@ endwhile; ?>
   <?php endif; ?>
 </section>
 
+<section class="c-page-section ta-c">
+  <div class="mw-64ex mb-1">
+    <h2 class="mb-1"><?php the_field('event_support_title'); ?></h2>
+    <p><?php the_field('event_support_text'); ?></p>
+  </div>
+
+  <?php if( have_rows('partner')): ?>
+  <ul class="d-f jc-c fw-w mt-3">
+    <?php
+    while( have_rows('partner')): the_row(); ?>
+      <li class="ml-2">
+        <a href="<?php the_sub_field('partner_link'); ?>"
+           title="Zur Website von <?php the_sub_field('partner_name'); ?>">
+          <?php
+          $foo = wp_get_attachment_image_src(get_sub_field('partner_img'), 'events-teaser-highdpi'); ?>
+          <img src="<?php echo $foo[0] ?>"
+               alt="Logo von <?php the_sub_field('partner_name'); ?>"
+               height="180">
+        </a>
+      </li>
+    <?php endwhile; ?>
+  </ul>
+  <?php
+  endif; ?>
+</section>
+
 
 <?php
+endwhile;
+
+
 get_footer();
