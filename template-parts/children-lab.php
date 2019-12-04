c583493974ac8bdf932b5e14f1b50025dc0c0a6f
lislis <mail@lislis.de>
2019-12-04 15:43:59 +0100

    random lab illu

--- template-parts/children-lab.php
+++ template-parts/children-lab.php
@@ -11,11 +11,18 @@
 <li>
   <article class="c-compact-teaser">
     <a href="<?php the_permalink() ?>" class="hover-line-trigger">
-      <div class="teaser-image">
+      <div class="teaser-image p-r">
         <picture>
           <img src="<?php echo get_the_post_thumbnail_url($post->ID, 'blog-large'); ?>" alt="">
         </picture>
-        <?php if (get_field('illustration')): ?><img src="<?php echo the_field('illustration') ?>" alt=""><?php endif; ?>
+        <div class="c-page-alpaca-friend--small">
+        <?php
+        $svg = get_random_illustration();
+        echo replace_svg_css_class_fill(get_svg_content($svg),
+                                        "changecolor",
+                                        "event-". get_the_ID() ."-" . $svg,
+                                        get_field('event_color')); ?></div>
+
       </div>
       <h2 class="teaser-title"><span class="hover-line"><?php the_title() ?></span></h2>
       <div class="teaser-summary"><?php the_excerpt() ?></div>
