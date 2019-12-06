7b788666212e95faa3ee44fa909c6e5bad2e8229
lislis <mail@lislis.de>
2019-12-06 15:43:09 +0100

    add newsletter link

--- template-parts/newsletter-footer.php
+++ template-parts/newsletter-footer.php
@@ -4,5 +4,5 @@
     <p><?php echo __('Jugend hackt will Jugendliche im verantwortungsbewussten Umgang mit Technik bestärken, um damit Lösungen für gesellschaftspolitische Fragen zu finden. Regelmäßige Updates bekommt ihr', 'lauch') ?></p>
   </div>
   <?php get_template_part( 'template-parts/social-media-list', get_post_type() ); ?>
-  <p>oder in unserem <a href="/anmeldung/">Newsletter</a></p>
+  <p>oder in unserem <a href="/newsletter/">Newsletter</a></p>
 </aside>
