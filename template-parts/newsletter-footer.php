4a24cc5b2e505b5adf25df5342e5386026a7ed0c
Philip Steffan <philip.steffan@okfn.de>
2020-04-09 11:17:33 +0200

    Update newsletter-footer.php

--- template-parts/newsletter-footer.php
+++ template-parts/newsletter-footer.php
@@ -1,8 +1,8 @@
 <aside class="c-breakbox c-breakbox--blue">
   <div class="c-breakbox-inner c-breakbox--blue">
     <h3><?php echo __('Mit Code die Welt verbessern', 'lauch'); ?></h3>
-    <p><?php echo __('Wir sind ein Programm für junge Menschen, die mit ihren technischen Fähigkeiten die Welt verbessern wollen. Regelmäßige Updates bekommt ihr', 'lauch') ?></p>
+    <p><?php echo __('Wir sind ein Programm für junge Menschen, die mit ihren technischen Fähigkeiten die Welt verbessern wollen. Folgt uns auf', 'lauch') ?></p>
   </div>
   <?php get_template_part( 'template-parts/social-media-list', get_post_type() ); ?>
-  <p>oder in unserem <a href="/newsletter/">Newsletter</a></p>
+  <p>oder abonniert unseren Newsletter per <a href="/newsletter/">E-Mail</a> oder <a target="_blank" rel="noreferrer noopener" href="https://t.me/jugendhackt">Telegram</a>.</p>
 </aside>
