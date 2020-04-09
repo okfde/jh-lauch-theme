d55dfd06f11b85cb4bf42e23b9b39ad24393d545
Philip Steffan <philip.steffan@okfn.de>
2020-04-09 11:33:48 +0200

    New social media icons

--- template-parts/social-media-list.php
+++ template-parts/social-media-list.php
@@ -2,17 +2,19 @@
   <li><a href="https://twitter.com/jugendhackt"
          title="Jugend hackt bei Twitter">
     <?php get_template_part('images/icons/icon', 'twitter.svg' ); ?></a></li>
+  <li><a href="https://chaos.social/@jugendhackt"
+         title="Jugend hackt bei chaos.social">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/icons/mastodon.png" alt=""></a></a></li>  
   <li><a href="https://www.instagram.com/jugendhackt/"
          title="Jugend hackt bei Instagram">
     <?php get_template_part('images/icons/icon', 'instagram.svg' ); ?></a></li>
-
   <li><a href="https://www.youtube.com/jugendhackt"
          title="YouTube Kanal von Jugend hackt">
     <?php get_template_part('images/icons/icon', 'youtube.svg' ); ?></a></li>
   <li><a href="https://www.facebook.com/jugendhackt"
          title="Jugend hackt Freundeskreis auf Facebook">
     <?php get_template_part('images/icons/icon', 'facebook.svg' ); ?></a></li>
-  <li><a href="https://community.jugendhackt.org/"
-         title="Zulip Online Community">
-    <?php render_svg('/images/icons/contact-zulip.svg'); ?></a></li>
+  <li><a href="https://t.me/jugendhackt"
+         title="Jugend hackt Freundeskreis auf Telegram">
+    <?php get_template_part('images/icons/icon', 'telegram.svg' ); ?></a></li>
 </ul>
