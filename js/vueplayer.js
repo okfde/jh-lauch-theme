93757000603644c907a7f3ffc7730e329804cf54
Philipp Veller <vellerphilipp@gmail.com>
2020-04-20 17:46:43 +0200

    fix video player shortcode

--- js/vueplayer.js
+++ js/vueplayer.js
@@ -61,7 +61,7 @@ document.addEventListener('DOMContentLoaded', function () {
       },
       computed: {
         playerSrc () {
-          return 'https://www.youtube-nocookie.com/embed/' + this.activeVideo.youtubeid + '?enablejsapi=1';
+          return 'https://www.youtube-nocookie.com/embed/' + this.activeVideo.youtubeid + '?enablejsapi=1&rel=0';
         },
         playerSrcdoc () {
           return `<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube-nocookie.com/embed/${this.activeVideo}?autoplay=1><img src=https://img.youtube.com/vi/${this.activeVideo}/hqdefault.jpg alt=''><span>▶</span></a>`;
