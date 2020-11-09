9929fa31371f392bac99093d492baad9fac7ec4c
Philip Steffan <philip.steffan@okfn.de>
2020-11-09 11:28:30 +0100

    zoom fix for new HD tiles

--- template-parts/current-exchange.php
+++ template-parts/current-exchange.php
@@ -172,7 +172,9 @@
 
  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+   tileSize: 512,
    maxZoom: 18,
+   zoomOffset: -1,
    id: 'mapbox/streets-v11',
    accessToken: 'pk.eyJ1Ijoib2tmZGUiLCJhIjoiY2syMzZjazFrMDZpejNtcW11Mm5pMnozNSJ9.6QaPMbeUg3k8fcVUmvvpxA'
  }).addTo(mymap);
