182a2a169ba467c4e3f3e7a300c4cf1b43e89104
Philip Steffan <philip.steffan@okfn.de>
2021-05-18 11:59:14 +0200

    Alpaca header for page

--- taxonomy-oer-topics.php
+++ taxonomy-oer-topics.php
@@ -5,12 +5,25 @@
 get_header();
 ?>
 
-  <div class="p-r">
-    <header class="c-page-offcenter-header">
-      <h1 class="c-page-title">Projekte zum Thema <?= get_queried_object()->name;?></h1>
-      <div class="c-page-excerpt"><?= get_queried_object()->description; ?></div>
-    </header>
-  </div>
+<header class="c-page-alpaca-header">
+    <div class="c-page-alpaca-featured medium-up as-s p-r">
+        <img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'blog-alpaka'); ?>"
+             alt="" class="clip-alpaka">
+    </div>
+    <div class="c-page-alpaca-title">
+        <nav class="c-breadcrumb" aria-label="breadcrumb">
+            <ol>
+                <li>
+                    <a href="<?php echo get_post_type_archive_link('oer'); ?>">OER</a>
+                </li>
+            </ol>
+        </nav>
+        <h1 class="c-page-title pt-1"><?= get_queried_object()->name;?></h1>
+        <div class="c-page-excerpt">
+            <?= get_queried_object()->description; ?>
+        </div>
+    </div>
+</header>
 
   <section class="c-page-section c-project-list pb-0 pt-0">
     <ul>
