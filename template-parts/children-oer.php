b329cd7fdd17bc312a4bc974e4bc48f07db44aa0
Philip Steffan <philip.steffan@okfn.de>
2022-02-16 12:47:39 +0100

    Update children-oer.php

--- template-parts/children-oer.php
+++ template-parts/children-oer.php
@@ -15,14 +15,7 @@
         <picture>
           <img src="<?php echo get_the_post_thumbnail_url($post->ID, 'blog-large'); ?>" alt="">
         </picture>
-        <div class="c-page-alpaca-friend--small">
-        <?php
-        $svg = get_random_illustration();
-        echo replace_svg_css_class_fill(get_svg_content($svg),
-                                        "changecolor",
-                                        "event-". get_the_ID() ."-" . $svg,
-                                        get_field('event_color')); ?></div>
-
+        <?php if (get_field('illustration')): ?><img src="<?php echo the_field('illustration') ?>" alt="" loading="lazy"><?php endif; ?>
       </div>
       <h2 class="teaser-title"><span class="hover-line"><?php the_title() ?></span></h2>
       <div class="teaser-summary"><?php the_excerpt() ?></div>
