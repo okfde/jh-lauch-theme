940a8a0e34d84d7e87ec307f928ced665b4199a1
Philip Steffan <philip.steffan@okfn.de>
2022-06-15 15:12:56 +0200

    Update partner-lab.php
    Improve logos on Lab pages

--- template-parts/partner-lab.php
+++ template-parts/partner-lab.php
@@ -8,17 +8,19 @@
     <ul class="d-f jc-c fw-w mt-3">
       <?php
       while( have_rows('partner')): the_row(); ?>
-        <li class="ml-3">
-          <a href="<?php the_sub_field('partner_link'); ?>"
-             title="Zur Website von <?php the_sub_field('partner_name'); ?>">
-            <?php
-            $image = wp_get_attachment_image_src(get_sub_field('partner_img'), 'partner-teaser'); ?>
-            <img src="<?php echo $image[0] ?>"
-                 alt="Logo von <?php the_sub_field('partner_name'); ?>"
-                 height="55">
-          </a>
-        </li>
-      <?php endwhile; ?>
+      <?php
+      $image = wp_get_attachment_image_src(get_sub_field('partner_img'), 'partner-teaser'); ?>
+      <li class="c-displayitem mr-2">
+        <a href="<?php the_sub_field('partner_link'); ?>"
+           title="Zur Website von <?php the_sub_field('partner_name'); ?> "
+           class="hover-line-trigger">
+          <img src="<?php echo $image[0] ?>" alt="" class="white">
+          <p class="c-displayitem-title">
+            <span class="hover-line"><?php the_sub_field('partner_name'); ?></span>
+          </p>
+        </a>
+      </li>
+    <?php endwhile ?>
     </ul>
   <?php
   endif; ?>
