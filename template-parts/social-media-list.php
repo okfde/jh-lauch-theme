ab9239fde096baaf27bcf74deb27af62e49662c6
Philip Steffan <philip.steffan@okfn.de>
2025-02-11 15:39:37 +0100

    Twitter raus, TikTok rein

--- template-parts/social-media-list.php
+++ template-parts/social-media-list.php
@@ -1,10 +1,10 @@
 <ul class="c-breakbox-list">
-  <li><a href="https://twitter.com/jugendhackt"
-         title="Jugend hackt bei Twitter">
-    <?php get_template_part('images/icons/icon', 'twitter.svg' ); ?></a></li>
   <li><a href="https://chaos.social/@jugendhackt"
          title="Jugend hackt bei chaos.social">
     <img src="<?php echo get_template_directory_uri(); ?>/images/icons/mastodon.png" height="18" width="18" alt="Mastodon"></a></a></li>
+  <li><a href="https://tiktok.com/@jugendhackt"
+         title="Jugend hackt bei TikTok">
+    <?php get_template_part('images/icons/icon', 'tiktok.svg' ); ?></a></li>
   <li><a href="https://www.instagram.com/jugendhackt/"
          title="Jugend hackt bei Instagram">
     <?php get_template_part('images/icons/icon', 'instagram.svg' ); ?></a></li>
