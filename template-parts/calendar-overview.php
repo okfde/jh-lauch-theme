a73321a379b9636458bedc6fa8a52974ab487b62
lislis <mail@lislis.de>
2026-05-11 15:58:29 +0200

    add pinning and different event sorting

--- template-parts/calendar-overview.php
+++ template-parts/calendar-overview.php
@@ -1,44 +1,51 @@
 <?php
     $num = $args['num'];
-    $eventp = get_posts(array('post_type' => 'page',
-                              'meta_query' => array(
-                                  array('key' => '_wp_page_template',
-                                        'value' => 'event-overview.php'))))[0];
-    $args = array('post_type' => 'page',
-                  'post_parent' => $eventp->ID,
-                  'meta_key' => 'is_active',
-                  'meta_value' => 1,
-                  'posts_per_page' => -1
-    );
-    $event_query = new WP_Query( $args );
 
     $args = array(
         'post_type' => 'date',
-        'orderby' => 'meta_value_datetime',
+        //'orderby' => 'meta_value_datetime',
         'meta_key' => 'begin',
         'meta_query' => post_date_get_timed_query(),
+        'orderby'           => 'meta_value',
         'posts_per_page' => $num,
-        'tax_query' => array(
-            array(
-                'taxonomy' => 'lab-location',
-                'field'    => 'slug',
-                'terms'    => array('online-community'), // set in functions.php
-                'operator' => 'NOT IN', // this line excludes the community
-            ),
-        )
+        'order'             => 'ASC',
+        /* 'tax_query' => array(
+         *     array(
+         *         'taxonomy' => 'lab-location',
+         *         'field'    => 'slug',
+         *         'terms'    => array('online-community'), // set in functions.php
+         *         'operator' => 'NOT IN', // this line excludes the community
+         *     ),
+         * ) */
     );
-    $lab_query = new WP_Query( $args );
+    $lab_events = get_posts( $args );
+    $lab_pinned = array_filter($lab_events, "filter_lab_pinned_events");
+    $lab_unpinned = array_filter($lab_events, "filter_lab_unpinned_events");
 
-    $ids = array_merge($event_query->posts, $lab_query->posts); ?>
+    function filter_lab_pinned_events($v) {
+        return get_field('pinned', $v->ID) === true;
+    }
+    function filter_lab_unpinned_events($v) {
+        return !filter_lab_pinned_events($v);
+    }
+
+    $lab_ordered = array_merge($lab_pinned, $lab_unpinned);
+
+    if( $posts ):
+?>
 
 <ul class="c-event-list__list mb-3">
-    <?php foreach ($ids as $post): ?>
+    <?php foreach ($lab_ordered as $post):
+        setup_postdata( $post );
+    ?>
         <li>
-            <?php if ($post->post_type == "date"):
+            <?php
                 get_template_part('template-parts/calendar', 'lab');
-                else:
-                get_template_part('template-parts/calendar', 'events');
-                endif; ?>
+            ?>
         </li>
     <?php endforeach; ?>
 </ul>
+<?php wp_reset_postdata(); ?>
+
+
+<?php endif; ?>
